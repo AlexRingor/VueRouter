8eb89ca51fd07a283f91221d5b5df71f63b0ad82
Alex Ringor <alexanderringor@gmail.com>
2023-10-23 19:17:55 +0800

    solution #1 when reacting to parameter changes

--- vue-router-vite-only/vue-router-school-travel/src/views/DestinationShow.vue
+++ vue-router-vite-only/vue-router-school-travel/src/views/DestinationShow.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="destination">
+    <section v-if="destination" class="destination">
         <h2>{{destination.name}}</h2>
         <div class="destination-details">
             <img :src="`/images/${destination.image}`" :alt="destination.name">
@@ -11,14 +11,32 @@
 <script>
 import sourceData from '../data.json'
 export default {
+    data() {
+        return {
+            destination: null
+        }
+    },
+    methods: {
+        async initData() {
+            const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`)
+            this.destination = await response.json()
+        }
+    },
     computed: {
         destinationId() {
             return parseInt(this.$route.params.id)
         },
-        destination() {
-            return sourceData.destinations.find(destination => destination.id === this.destinationId)
+    },
+    
+    async created() {
+            this.initData()
+            this.$watch(
+                () => this.$route.params,
+                async() => {
+                    this.initData()
+                }
+            )
         }
-    }
         
 }
           
