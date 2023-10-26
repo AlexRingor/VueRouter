2c58cc3a35e7fd7cccb7f3519446f28ce82bd6a7
Alex Ringor <alexanderringor@gmail.com>
2023-10-26 21:28:26 +0800

    Router Query Params

--- vue-router-vite-only/vue-router-school-travel/src/views/Protected.vue
+++ vue-router-vite-only/vue-router-school-travel/src/views/Protected.vue
@@ -3,6 +3,9 @@
         <h1>Greetings, {{username}}</h1>
         <button class="btn logout" @click="logout">Logout</button>
     </div>
+    <router-link :to="{name: 'invoices'}">
+        <button>Invoices</button>
+    </router-link>
 </template>
 
 <script>
