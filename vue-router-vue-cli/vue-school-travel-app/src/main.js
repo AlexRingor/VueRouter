17a70e092b85305fc68d4d85a789726fc98fe8f8
Alex Ringor <alexanderringor@gmail.com>
2023-10-30 13:51:43 +0800

    --no-edit

--- vue-router-vue-cli/vue-school-travel-app/src/main.js
+++ vue-router-vue-cli/vue-school-travel-app/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import AppLink from './components/AppLink.vue'
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+.component( 'Applink', AppLink)
+.use(router)
+.mount("#app");
