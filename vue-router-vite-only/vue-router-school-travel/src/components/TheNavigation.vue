9a8f9d787614683b6179efe2141ce5f1e6c5cdc7
Alex Ringor <alexanderringor@gmail.com>
2023-10-30 14:00:58 +0800

    test push

--- vue-router-vite-only/vue-router-school-travel/src/components/TheNavigation.vue
+++ vue-router-vite-only/vue-router-school-travel/src/components/TheNavigation.vue
@@ -8,6 +8,7 @@
       </AppLink>
       <AppLink :to="{name: 'protected'}">Dashboard</AppLink>
       <AppLink to="https://google.com">Google.com</AppLink>
+      <h1>Test</h1>
     </div>
 </template>
 
